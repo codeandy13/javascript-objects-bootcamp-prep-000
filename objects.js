619e83ae6d6c48380e0669e785467fbd4522488a
Andrew Kim <akim618@gmail.com>
2018-10-31 21:45:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   SamSmith: "Lay me Down",
   EdSheeran: "Shape of You"
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, {[artistName]: songTitle} )
+}
