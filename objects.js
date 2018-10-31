815e78511675ece1999ede77223f363dcf555ec8
Andrew Kim <akim618@gmail.com>
2018-10-31 22:57:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return object.assign(delete artistName)
-  
+  return object.assign(delete playlist.artistName)
+
 }
